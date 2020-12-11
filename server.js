8e1547ed96ed3ec569212dec22e1864710bc2160
stormlilian <lilianc.seliga@gmail.com>
2020-12-10 23:31:40 -0300

    Edita porta padrao heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require("./src/app")
-const PORT = 3030
 
-app.listen(PORT, function(){
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, function(){
     console.log(`Bebe o APP ta rodando na porta ${PORT}`)
 })
