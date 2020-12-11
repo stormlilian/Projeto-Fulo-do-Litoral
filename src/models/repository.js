b20bc4734b0d6b460117e567f3298c44cf0b06a2
stormlilian <lilianc.seliga@gmail.com>
2020-12-11 01:22:00 -0300

    Adiciona arquivos de deploy

--- src/models/repository.js
+++ src/models/repository.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose")
 const DB_URL = "mongodb://localhost:27017/db-artesao"
 
+const uri = process.env.MONGO_DB_URI || 'mongodb+srv://admin:lokije@clusterlilian.l2vwe.mongodb.net/artesa?retryWrites=true&w=majority';
+
 const connect = ()=>{
-    mongoose.connect(DB_URL, { useUnifiedTopology: true })
+    mongoose.connect(uri, { useUnifiedTopology: true })
     const connection = mongoose.connection
     connection.on('error', () => console.error('Erro ao se conectar'))
     connection.once('open',  () => console.log('Conectamos no Mongo'))
